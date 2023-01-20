ac1fa48ac46d4fb12215fd0b518fcd7615708f3e
Natalia B <90588475+nanab210@users.noreply.github.com>
2023-01-20 16:02:21 -0300

    Fetch Repo Data

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const overview = document.querySelector(".overview");
 // add Github username
 const username = "nanab210";
+//select unordered list to display the repos list
+const displayRepos = document.querySelector(".repo-list");
 
 const gitProfile = async function () {
   const userInfo = await fetch(`https://api.github.com/users/${username}`);
@@ -28,6 +30,24 @@ const displayUserInfo = function (data) {
       <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
     </div> 
 `;
-
   overview.append(div);
+  listOfRepos();
+};
+
+const listOfRepos = async function () {
+  const fetchRepo = await fetch(
+    `https://api.github.com/users/${username}/repos?sort=updated&per_page=100}`
+  );
+  const repoData = await fetchRepo.json();
+  //console.log(repoData);
+  displayRepoInfo(repoData);
+};
+
+const displayRepoInfo = function (repos) {
+  for (repo of repos) {
+    const repoItem = document.createElement("li");
+    repoItem.classList.add("repo");
+    repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+    displayRepos.append(repoItem);
+  }
 };
